078c41a8861516ccc9b0ff876de14d90d1014981
Arturo Borrero Gonzalez <arturo@debian.org>
2016-11-17 17:03:53 +0100

    d/tests/: disable internaltest-py.sh
    Missing dummy kernel module. It's being discussed with upstream.
    
    Signed-off-by: Arturo Borrero Gonzalez <arturo@debian.org>

--- debian/tests/internaltest-py.sh
+++ debian/tests/internaltest-py.sh
@@ -2,6 +2,9 @@
 
 # Run the internal tests of nftables (py)
 
+echo "W: Not running this testsuite due to missing dummy module"
+exit 0
+
 # The testsuite requires kernel at least 4.x
 if [ "$(uname -r | cut -d. -f1)" -lt 4 ] ; then
 	echo "W: This testsuite is likely to produce many fails because of old kernel"
